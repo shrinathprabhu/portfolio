89f478b2e17d4f4204c39dc0955f82ecfb136183
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2023-02-24 16:53:02 +0530

    Initial commit

---
+++ App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <main>
+      <section aria-label="details">Details</section>
+      <section aria-label="illustration">
+        <nuxt-img src="@/assets/images/web-developer-illustration.png" />
+      </section>
+    </main>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default {
+  image: {},
+  target: "static",
+  buildModules: ["@nuxt/image"],
+};
