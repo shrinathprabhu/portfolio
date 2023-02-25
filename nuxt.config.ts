f3bcd58eda17987276fb8c9541baa24a4641a040
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2023-02-25 22:56:31 +0530

    Update head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,31 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default {
-  image: {},
-  target: "static",
-  buildModules: ["@nuxt/image"],
-};
+export default defineNuxtConfig({
+  modules: ["@morev/vue-transitions/nuxt"],
+  app: {
+    head: {
+      title: "Web Developer - Shrinath Prabhu",
+      link: [
+        {
+          rel: "stylesheet",
+          href: "/css/styles.css",
+        },
+        {
+          rel: "icon",
+          href: "/favicon.svg",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Russo+One&family=Noto+Sans:wght@300;400;500;600&display=swap",
+        },
+      ],
+    },
+  },
+});
