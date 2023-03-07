d1f6a873654629f4e491ac433ace087a067d259f
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2023-03-07 20:26:42 +0530

    Test page navigation

---
+++ error.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Custom 404</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,479 @@
+<script setup lang="ts">
+import gsap from "gsap";
+
+const showModal = ref(false);
+
+const works = [
+  {
+    tooltip: "Arcana Network - since 2021",
+    logo: {
+      alt: "Arcana Network",
+      src: "/images/arcana.png",
+      webp: "/images/webp/arcana.webp",
+      width: "160",
+      height: "127",
+    },
+    link: {
+      src: "https://www.arcana.network/",
+      rel: "noopener",
+    },
+  },
+  {
+    tooltip: "Hashbinary Services - 2018-2021",
+    logo: {
+      alt: "Hashbinary Services LLP",
+      src: "/images/hashbinary.png",
+      webp: "/images/webp/hashbinary.webp",
+      width: "426",
+      height: "400",
+    },
+    link: {
+      src: "https://hashbinary.com/",
+      rel: "noopener",
+    },
+  },
+];
+
+const skills = [
+  {
+    logo: "/images/vue.svg",
+    name: "Vue",
+  },
+  {
+    logo: "/images/nuxt-dot-js.svg",
+    name: "Nuxt",
+  },
+  {
+    logo: "/images/node-js.svg",
+    name: "Node.js",
+  },
+  {
+    logo: "/images/javascript.svg",
+    name: "Javascript",
+  },
+  {
+    logo: "/images/typescript.svg",
+    name: "Typescript",
+  },
+  {
+    logo: "/images/express.svg",
+    name: "Express",
+  },
+  {
+    logo: "/images/mongodb.svg",
+    name: "MongoDB",
+  },
+  {
+    logo: "/images/html.svg",
+    name: "HTML 5",
+  },
+  {
+    logo: "/images/css3.svg",
+    name: "CSS 3",
+  },
+  {
+    logo: "/images/tailwind-css.svg",
+    name: "Tailwind",
+  },
+  {
+    logo: "/images/wordpress.svg",
+    name: "Wordpress",
+  },
+];
+
+useHead({
+  script: [
+    {
+      children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;j.defer=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PLJ5D24');`,
+    },
+  ],
+});
+
+function animateSkills(
+  animatedSkillLogos: NodeListOf<Element>,
+  animatedSkillTexts: NodeListOf<Element>,
+  index: number
+) {
+  gsap
+    .timeline({ delay: 0.3 })
+    .fromTo(
+      animatedSkillLogos[index],
+      { x: 65 },
+      {
+        scale: 1,
+        x: 0,
+        opacity: 1,
+        duration: 0.3,
+      }
+    )
+    .to(animatedSkillTexts[index], { opacity: 1, duration: 0.3 })
+    .to(
+      animatedSkillLogos[index],
+      {
+        scale: 0,
+        opacity: 0,
+        x: -65,
+        duration: 0.3,
+        delay: 1,
+        onComplete: animateSkills,
+        onCompleteParams: [
+          animatedSkillLogos,
+          animatedSkillTexts,
+          index + 1 === animatedSkillLogos.length ? 0 : index + 1,
+        ],
+      },
+      "-=0.1"
+    )
+    .to(animatedSkillTexts[index], { opacity: 0, duration: 0.3 }, "-=0.3");
+}
+
+onMounted(() => {
+  const gsapStartDelay = window.innerWidth < 768 ? 10000 : 8000;
+  const animatedSkillLogos = document.querySelectorAll(
+    ".animated-skill-logo-container"
+  );
+  const animatedSkillTexts = document.querySelectorAll(".animated-skill-text");
+  setTimeout(
+    () => animateSkills(animatedSkillLogos, animatedSkillTexts, 0),
+    gsapStartDelay
+  );
+});
+</script>
+
+<template>
+  <main>
+    <Html lang="en"></Html>
+    <div class="home-page-main">
+      <section aria-label="details" class="flex-col" style="gap: 2rem">
+        <div class="flex-col" style="gap: 0.25rem">
+          <div class="fade-in-up">
+            <h1 class="text-shadow dev-designation">Web Developer</h1>
+          </div>
+          <span
+            class="fade-in-up"
+            style="
+              font-size: var(--fs-xbase);
+              animation-delay: var(--duration-fade-in);
+            "
+          >
+            who can help create websites, web apps, and APIs for your business.
+          </span>
+        </div>
+        <div>
+          <span
+            class="section-heading text-shadow fade-in"
+            style="animation-delay: calc(1.5 * var(--duration-fade-in))"
+            >Why you'll need me</span
+          >
+          <ul class="flex-col">
+            <li
+              class="fade-in-right"
+              style="animation-delay: calc(1.8 * var(--duration-fade-in))"
+            >
+              You have a design and want to convert it into a fully functional
+              website or a web app.
+            </li>
+            <li
+              class="fade-in-right"
+              style="animation-delay: calc(2.1 * var(--duration-fade-in))"
+            >
+              You want to create API endpoints for your project in Node.js.
+            </li>
+            <li
+              class="fade-in-right"
+              style="animation-delay: calc(2.4 * var(--duration-fade-in))"
+            >
+              You want someone who can help you with both the frontend and the
+              backend.
+            </li>
+          </ul>
+        </div>
+        <div class="flex-col" style="gap: 0.5rem">
+          <span
+            class="section-heading text-shadow fade-in"
+            style="animation-delay: calc(3 * var(--duration-fade-in))"
+            >Worked for
+          </span>
+          <div class="flex" style="gap: 0.75rem">
+            <VTooltip
+              v-for="(work, index) in works"
+              :key="work.tooltip"
+              :text="work.tooltip"
+            >
+              <a
+                class="flex work-link fade-in"
+                :href="work.link.src"
+                target="_blank"
+                :rel="work.link.rel"
+                :style="`animation-delay: calc(${
+                  3 + (index + 1) * 0.35
+                } * var(--duration-fade-in))`"
+              >
+                <picture>
+                  <source
+                    type="image/webp"
+                    :srcset="work.logo.webp"
+                    :width="work.logo.width"
+                    :height="work.logo.height"
+                  />
+                  <img
+                    class="work-logo"
+                    :src="work.logo.src"
+                    :alt="work.logo.alt"
+                    :width="work.logo.width"
+                    :height="work.logo.height"
+                  />
+                </picture>
+              </a>
+            </VTooltip>
+          </div>
+        </div>
+        <div class="flex-col" style="gap: 0.75rem; align-items: start">
+          <div class="flex" style="gap: 1.5rem; align-items: center">
+            <button
+              id="lets-connect"
+              class="cta-button fade-in-zoom"
+              aria-haspopup="dialog"
+              style="animation-delay: calc(3.75 * var(--duration-fade-in))"
+              @click.stop="showModal = true"
+            >
+              Let's Connect
+            </button>
+            <div class="flex icons" style="gap: 1.125rem">
+              <VTooltip text="See Shrinath's code on Github">
+                <a
+                  class="fade-in"
+                  href="https://github.com/shrinathprabhu"
+                  target="_blank"
+                  rel="noopener"
+                  aria-label="See Shrinath's code on Github"
+                  style="animation-delay: calc(4 * var(--duration-fade-in))"
+                >
+                  <IconsGithub class="social-icon" />
+                </a>
+              </VTooltip>
+              <VTooltip text="Find Shrinath on Linkedin">
+                <a
+                  class="fade-in"
+                  href="https://www.linkedin.com/in/shrinath-prabhu"
+                  target="_blank"
+                  rel="noopener"
+                  aria-label="Find Shrinath on Linkedin"
+                  style="animation-delay: calc(4.25 * var(--duration-fade-in))"
+                >
+                  <IconsLinkedin class="social-icon" />
+                </a>
+              </VTooltip>
+              <VTooltip text="Find Shrinath on Twitter">
+                <a
+                  class="fade-in"
+                  href="https://twitter.com/shrinath_prabhu"
+                  target="_blank"
+                  rel="noopener"
+                  aria-label="Find Shrinath on Twitter"
+                  style="animation-delay: calc(4.5 * var(--duration-fade-in))"
+                >
+                  <IconsTwitter class="social-icon" />
+                </a>
+              </VTooltip>
+            </div>
+          </div>
+          <div class="flex" style="margin-top: 1rem">
+            <a
+              id="read-my-blog"
+              href="https://medium.com/arcana-network-blog/vue-3-composition-api-basics-and-patterns-44813f2c785d"
+              target="_blank"
+              rel="noopener"
+              class="link-button fade-in-right"
+              style="animation-delay: calc(3.85 * var(--duration-fade-in))"
+              >Read my blog</a
+            >
+          </div>
+        </div>
+      </section>
+      <section aria-label="illustration" class="flex center">
+        <div class="hero-image-container fade-in-zoom">
+          <div
+            v-for="skill in skills"
+            :key="skill.name"
+            class="animated-skill-logo-container"
+          >
+            <img
+              class="animated-skill-logo"
+              :src="skill.logo"
+              :alt="`Can also code in ${skill.name}`"
+              loading="lazy"
+            />
+            <span class="animated-skill-text">{{ skill.name }}</span>
+          </div>
+          <picture class="illustration">
+            <IllustrationWebDev aria-label="Web Developer Illustration" />
+          </picture>
+        </div>
+      </section>
+    </div>
+    <VModal :show="showModal" @close="showModal = false">
+      <ContactForm />
+    </VModal>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  height: calc(100% - 4rem);
+  align-items: flex-start;
+  justify-content: center;
+  padding-block: 2rem;
+}
+
+.home-page-main {
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+  overflow-x: hidden;
+  padding-inline: 2rem;
+}
+
+.dev-designation {
+  background: linear-gradient(
+    to right,
+    var(--color-accent),
+    var(--color-accent-light),
+    var(--color-text),
+    var(--color-accent-light),
+    var(--color-accent),
+    var(--color-accent-light),
+    var(--color-text),
+    var(--color-accent-light),
+    var(--color-accent)
+  );
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-size: 800% 100%;
+  animation: textGradientMotion 30s ease infinite;
+}
+
+@keyframes textGradientMotion {
+  0% {
+    background-position: 0% 100%;
+  }
+  50% {
+    background-position: 100% 100%;
+  }
+  100% {
+    background-position: 0% 100%;
+  }
+}
+
+.section-heading {
+  font-size: var(--fs-lg);
+  font-weight: 600;
+}
+
+li {
+  margin-top: 0.5rem;
+}
+
+.work-link {
+  width: 2rem;
+  aspect-ratio: 1/1;
+  padding: 0.25rem;
+  transition: transform var(--duration-base);
+}
+
+.work-logo,
+.social-icon {
+  object-fit: contain;
+  width: 100%;
+}
+
+.icons a {
+  display: flex;
+  width: 1.5rem;
+  cursor: pointer;
+  transition: all var(--duration-base);
+}
+
+.work-link:is(:hover, :focus-visible),
+.icons :is(a:hover, a:focus-visible) {
+  transform: scale(1.5);
+}
+
+.social-icon:is(:hover, :focus-visible) {
+  color: var(--color-accent-light);
+  filter: drop-shadow(1px 2px 10px var(--color-accent-light));
+}
+
+.hero-image-container {
+  position: relative;
+  animation-delay: calc(4.6 * var(--duration-fade-in));
+}
+
+.animated-skill-logo-container {
+  position: absolute;
+  width: 10%;
+  aspect-ratio: 1/1;
+  top: 40%;
+  left: 24%;
+  transform: scale(0);
+  opacity: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.375rem;
+}
+
+.animated-skill-logo {
+  position: relative;
+  width: 100%;
+  aspect-ratio: 1/1;
+  object-fit: contain;
+}
+
+.animated-skill-text {
+  position: relative;
+  font-size: var(--fs-sm);
+  color: var(--color-tooltip-bg);
+  font-weight: 600;
+  opacity: 0;
+}
+
+@media screen and (min-width: 768px) {
+  main {
+    align-items: center;
+    padding-inline: 2rem;
+  }
+  .home-page-main {
+    grid-template-columns: 1fr 1fr;
+    padding: 2rem;
+    isolation: isolate;
+    gap: 0.5rem;
+    backdrop-filter: blur(12px);
+  }
+
+  .home-page-main::before {
+    position: absolute;
+    content: "";
+    inset: 0;
+    background-color: var(--color-text);
+    opacity: 0.15;
+    border-radius: 2rem;
+    z-index: -1;
+  }
+
+  ul {
+    max-width: 480px;
+  }
+
+  .hero-image-container {
+    animation-delay: var(--duration-base);
+  }
+}
+</style>
